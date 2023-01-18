2881a0bf29c3ab3be34b5589b9160bcd7a208056
Alkida Vaci <alkida.vaci@gmail.com>
2023-01-17 21:05:43 -0500

    Added views folder

---
+++ public/js/listing.js
@@ -0,0 +1,30 @@
+const newHouseHandler = async (event) => {
+    event.preventDefault();
+  
+    const address = document.querySelector('#address').value.trim();
+    const city = document.querySelector('#city').value.trim();
+    const state = document.querySelector('#state').value.trim();
+    const zipCode = document.querySelector('#zip-code').value.trim();
+    const price = document.querySelector('#price').value.trim();
+  
+    if (address && city && state && zipCode && price) {
+      const response = await fetch(`/api/house`, {
+        method: 'POST',
+        body: JSON.stringify({ address, city, state, zipCode, price}),
+        headers: {
+          'Content-Type': 'application/json',
+        },
+      });
+  
+      if (response.ok) {
+        document.location.replace('/');
+      } else {
+        alert('Failed to create project');
+      }
+    }
+  };
+  
+   
+  document
+    .querySelector('.new-house-submit')
+    .addEventListener('submit', newHouseHandler);
