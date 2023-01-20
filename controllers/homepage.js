5ec667d11b6415c69097f9061095d0a57d27d979
dj3rry <davis.jerryjr@gmail.com>
2023-01-19 21:07:49 -0500

    updating route/controller folder structure

--- controllers/homepage.js
+++
@@ -1,6 +0,0 @@
-exports.homepage = async (req, res) => {
-    // Get data for the homepage
-    const data = await getHomepageData();
-    // Render the main.handlebars template with the data
-    res.render('main', { data });
-}
